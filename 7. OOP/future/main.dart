3d6cd330d6082fe6c1335773c1ec908961c3cb8f
Haziqbatak <haziqimtiyaz17@gmail.com>
2024-08-02 10:50:54 +0700

    new

--- 7. OOP/future/main.dart
+++ 7. OOP/future/main.dart
@@ -1,11 +1,32 @@
 void main(List<String> args) async {
-  print("Getting Your Coffee Order");
+  print("Getting Your Coffee Order...");
   var order = await getOrder();
   print("Your Coffee Order is: $order");
+
+  try {
+    var order = await getOrder();
+    print("Your Coffee Order is: $order");
+  } catch (e) {
+    print("Error: $e");
+  } finally {
+    print("Thank You For Ordering Coffee!");
+  }
+
+  //menjalankan error
+  try {
+    var order = await Future.error(Exception("Out Of Coffee"));
+    print("Your Coffee Order: $order");
+  } catch (e) {
+    //menangkap error
+    print("Error: $e");
+  } finally {
+    //selalu dijalankan
+    print("Thank You For Ordering Coffee!");
+  }
 }
 
 Future<String> getOrder() {
-  return Future.delayed(Duration(seconds: 3), () {
+  return Future.delayed(Duration(seconds: 2), () {
     return "Latte";
   });
 }
