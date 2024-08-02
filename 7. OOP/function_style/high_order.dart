b5efa972ae2df055041a938b783ef580f2a86680
Haziqbatak <haziqimtiyaz17@gmail.com>
2024-08-02 09:16:06 +0700

    new

---
+++ 7. OOP/function_style/high_order.dart
@@ -0,0 +1,19 @@
+void main(List<String> args) {
+  //function high order yang menerima parameterfungsi
+  //calculate sebagai nama fungsi
+  //int a, int b sebagai parameter ke-1 dan ke-2
+  //Function sebagai parameter ke-3
+  void calculate(int a, int b, Function operation) {
+    print("Result: ${operation(a, b)}");
+  }
+
+  //fungsi penjumlahan
+  int add(int a, int b) => a + b;
+
+  //fungsi pengurangan
+  int subtract(int a, int b) => a - b;
+
+  //memanggil fungsi high order
+  calculate(7, 3, add);
+  calculate(7, 3, subtract);
+}
